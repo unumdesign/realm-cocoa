262b601a2c0bb77b87e67339f056a51ab15c8db9
Jason Flax <jsflax@gmail.com>
2020-07-02 08:04:57 +0100

    make goroot and stitch_dir non const

--- Realm/ObjectServerTests/setup_baas.rb
+++ Realm/ObjectServerTests/setup_baas.rb
@@ -11,7 +11,7 @@ STITCH_VERSION='8322b26c33396ec3de1c0d8d7f8061e94a57b9ba'
 BASE_DIR = Dir.pwd
 BUILD_DIR = "#{BASE_DIR}/build"
 PID_FILE = "#{BUILD_DIR}/pid.txt"
-STITCH_DIR = "#{BUILD_DIR}/go/src/github.com/10gen/stitch"
+#stitch_dir = "#{BUILD_DIR}/go/src/github.com/10gen/stitch"
 
 MONGODB_URL="https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-#{MONGODB_VERSION}.tgz"
 TRANSPILER_TARGET='node8-macos'
@@ -60,74 +60,75 @@ end
 def setup_stitch
     puts "setting up stitch"
     exports = []
+    go_root = ''
+    stitch_dir = ''
 
-    GOROOT = ''
     if `which go`.empty? && !Dir.exists?("#{BUILD_DIR}/go")
         puts 'downloading go'
         `cd #{BUILD_DIR} && curl --silent "https://dl.google.com/go/go#{GO_VERSION}.darwin-amd64.tar.gz" | tar xz`
-        GOROOT = "#{BUILD_DIR}/go"
+        go_root = "#{BUILD_DIR}/go"
     else
-        GOROOT = `$GOROOT`
-        STITCH_DIR = '#{GOROOT}/src/github.com/10gen/stitch'
+        go_root = `$GOROOT`
+        stitch_dir = '#{go_root}/src/github.com/10gen/stitch'
     end
 
-    if !Dir.exists?(STITCH_DIR)
+    if !Dir.exists?(stitch_dir)
         puts 'cloning stitch'
         `git clone git@github.com:10gen/baas #{GOROOT}/src/github.com/10gen/stitch`
-        STITCH_DIR = "#{GOROOT}/src/github.com/10gen/stitch"
+        stitch_dir = "#{go_root}/src/github.com/10gen/stitch"
     end
 
     puts 'checking out stitch'
-    `git -C '#{STITCH_DIR}' fetch && git -C '#{STITCH_DIR}' checkout #{STITCH_VERSION}`
+    `git -C '#{stitch_dir}' fetch && git -C '#{stitch_dir}' checkout #{STITCH_VERSION}`
 
-    dylib_dir = "#{STITCH_DIR}/etc/dylib"
+    dylib_dir = "#{stitch_dir}/etc/dylib"
     if !Dir.exists?(dylib_dir)
         puts 'downloading mongodb dylibs'
         Dir.mkdir dylib_dir
         puts `curl -s "#{SERVER_STITCH_LIB_URL}" | tar xvfz - --strip-components=1 -C '#{dylib_dir}'`
     end
 
-    update_doc_filepath = "#{STITCH_DIR}/update_doc"
+    update_doc_filepath = "#{stitch_dir}/update_doc"
     if !File.exists?(update_doc_filepath)
         puts "downloading update_doc"
-        puts `cd '#{STITCH_DIR}' && curl --silent -O "https://s3.amazonaws.com/stitch-artifacts/stitch-mongo-libs/stitch_mongo_libs_osx_patch_cbcbfd8ebefcca439ff2e4d99b022aedb0d61041_59e2b7a5c9ec4432c400181c_17_10_15_01_19_33/update_doc"`
+        puts `cd '#{stitch_dir}' && curl --silent -O "https://s3.amazonaws.com/stitch-artifacts/stitch-mongo-libs/stitch_mongo_libs_osx_patch_cbcbfd8ebefcca439ff2e4d99b022aedb0d61041_59e2b7a5c9ec4432c400181c_17_10_15_01_19_33/update_doc"`
         puts `chmod +x '#{update_doc_filepath}'`
     end
 
-    assisted_agg_filepath = "#{STITCH_DIR}/assisted_agg"
+    assisted_agg_filepath = "#{stitch_dir}/assisted_agg"
     if !File.exists?(assisted_agg_filepath)
         puts "downloading assisted_agg"
-        puts `cd '#{STITCH_DIR}' && curl --silent -O "https://s3.amazonaws.com/stitch-artifacts/stitch-mongo-libs/stitch_mongo_libs_osx_patch_cbcbfd8ebefcca439ff2e4d99b022aedb0d61041_59e2b7ab2a60ed5647001827_17_10_15_01_19_39/assisted_agg"`
+        puts `cd '#{stitch_dir}' && curl --silent -O "https://s3.amazonaws.com/stitch-artifacts/stitch-mongo-libs/stitch_mongo_libs_osx_patch_cbcbfd8ebefcca439ff2e4d99b022aedb0d61041_59e2b7ab2a60ed5647001827_17_10_15_01_19_39/assisted_agg"`
         puts `chmod +x '#{assisted_agg_filepath}'`
     end
 
-    if `which node`.empty? && !Dir.exists?("#{STITCH_DIR}/node-v#{NODE_VERSION}-darwin-x64")
+    if `which node`.empty? && !Dir.exists?("#{stitch_dir}/node-v#{NODE_VERSION}-darwin-x64")
         puts "downloading node 🚀"
-        puts `cd '#{STITCH_DIR}' && curl -O "https://nodejs.org/dist/v#{NODE_VERSION}/node-v#{NODE_VERSION}-darwin-x64.tar.gz" && tar xzf node-v#{NODE_VERSION}-darwin-x64.tar.gz`
-        exports << "export PATH=\"#{STITCH_DIR}/node-v#{NODE_VERSION}-darwin-x64/bin/:$PATH\""
+        puts `cd '#{stitch_dir}' && curl -O "https://nodejs.org/dist/v#{NODE_VERSION}/node-v#{NODE_VERSION}-darwin-x64.tar.gz" && tar xzf node-v#{NODE_VERSION}-darwin-x64.tar.gz`
+        exports << "export PATH=\"#{stitch_dir}/node-v#{NODE_VERSION}-darwin-x64/bin/:$PATH\""
     end
 
     if `which yarn`.empty?
         `rm -rf "$HOME/.yarn"`
-        `export PATH=\"#{STITCH_DIR}/node-v#{NODE_VERSION}-darwin-x64/bin/:$PATH\" && curl -o- -L https://yarnpkg.com/install.sh | bash`
+        `export PATH=\"#{stitch_dir}/node-v#{NODE_VERSION}-darwin-x64/bin/:$PATH\" && curl -o- -L https://yarnpkg.com/install.sh | bash`
         exports << "export PATH=\"$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH\""
     end
 
     puts 'building transpiler'
     puts `#{exports.length() == 0 ? "" : exports.join(' && ') + ' &&'} \
-        cd '#{STITCH_DIR}/etc/transpiler' && yarn install && yarn run build -t "#{TRANSPILER_TARGET}"`
+        cd '#{stitch_dir}/etc/transpiler' && yarn install && yarn run build -t "#{TRANSPILER_TARGET}"`
 
-    exports << "export GOROOT=\"#{BASE_DIR}/build/go\""
+    exports << "export GOROOT=\"#{go_root}\""
     exports << "export PATH=\"$GOROOT/bin:$PATH\""
 
-    exports << "export STITCH_PATH=\"#{STITCH_DIR}\""
+    exports << "export STITCH_PATH=\"#{stitch_dir}\""
     exports << "export PATH=\"$PATH:$STITCH_PATH/etc/transpiler/bin\""
     exports << "export LD_LIBRARY_PATH=\"$STITCH_PATH/etc/dylib/lib\""
 
     puts 'build create_user binary'
 
     puts `#{exports.join(' && ')} && \
-        cd '#{STITCH_DIR}' && \
+        cd '#{stitch_dir}' && \
         go build -o create_user cmd/auth/user.go`
 
     puts 'create_user binary built'
@@ -135,7 +136,7 @@ def setup_stitch
     puts 'building server binary'
 
     puts `#{exports.join(' && ')} && \
-        cd '#{STITCH_DIR}' && \
+        cd '#{stitch_dir}' && \
         go build -o stitch_server cmd/server/main.go`
         
     puts 'server binary built'
@@ -157,7 +158,7 @@ end
 def clean_action
     puts 'cleaning'
     `rm -rf #{MONGO_DIR}`
-    `cd #{STITCH_DIR} && git rm -rf . && git clean -fxd`
+    `cd #{BUILD_DIR} && git rm -rf . && git clean -fxd`
 end
 
 if ARGV.length < 1
